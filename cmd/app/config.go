f55250cddaf0ad955e68082928b3a585f1a2d662
Georgi Pavlov <georgi.pavlov@sap.com>
2020-11-04 17:59:03 +0000

    support for parameterized manifests

--- cmd/app/config.go
+++ cmd/app/config.go
@@ -5,23 +5,44 @@
 package app
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 
 	"github.com/gardener/docforge/pkg/api"
 )
 
 // Manifest creates documentation model from configration file
-func Manifest(filePath string) *api.Documentation {
+func Manifest(filePath string, vars map[string]string) *api.Documentation {
 	var (
 		docs *api.Documentation
 	)
-	configBytes, err := ioutil.ReadFile(filePath)
+	blob, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		panic(fmt.Sprintf("%v\n", err))
 	}
-	if docs, err = api.Parse(configBytes); err != nil {
+	blob, err = resolveVariables(blob, vars)
+	if err != nil {
+		panic(fmt.Sprintf("%v\n", err))
+	}
+	if docs, err = api.Parse(blob); err != nil {
 		panic(fmt.Sprintf("%v\n", err))
 	}
 	return docs
+}
+
+func resolveVariables(manifestContent []byte, vars map[string]string) ([]byte, error) {
+	var (
+		tmpl *template.Template
+		err  error
+		b    bytes.Buffer
+	)
+	if tmpl, err = template.New("").Parse(string(manifestContent)); err != nil {
+		return nil, err
+	}
+	if err := tmpl.Execute(&b, vars); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
