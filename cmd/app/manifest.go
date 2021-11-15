ffabb8d93ebc7f6a3e4de57fe6b2874f881a50b3
Viktor <mail.victor.kostov@gmail.com>
2021-11-15 15:12:09 +0200

    fs removed

--- cmd/app/manifest.go
+++ cmd/app/manifest.go
@@ -7,18 +7,45 @@ package app
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/gardener/docforge/pkg/api"
 	"github.com/gardener/docforge/pkg/resourcehandlers"
+	"k8s.io/klog/v2"
 )
 
 // Manifest reads the resource at uri, resolves it as template applying vars,
 // and finally parses it into api.Documentation model
 func manifest(ctx context.Context, uri string, resourceHandlers []resourcehandlers.ResourceHandler) (*api.Documentation, error) {
-	var handler resourcehandlers.ResourceHandler
+	var (
+		handler         resourcehandlers.ResourceHandler
+		manifestContent []byte
+	)
 	uri = strings.TrimSpace(uri)
 	registry := resourcehandlers.NewRegistry(resourceHandlers...)
+
+	//chekc if uri is in file system
+	fileInfo, err := os.Stat(uri)
+	if err == nil {
+		//uri is from file system
+
+		if fileInfo.IsDir() {
+			return nil, fmt.Errorf("top level manifest %s is a directory", uri)
+		}
+		nVersions := api.ChooseNVersions(uri)
+		if nVersions > 0 {
+			klog.Warningf("There is a yaml file from file system not connected with a repository. Therefore LastNSupportedVersions is set to 0 for file %s", uri)
+		}
+		if manifestContent, err = ioutil.ReadFile(uri); err != nil {
+			return nil, err
+		}
+
+		targetBranch := api.ChooseTargetBranch(uri, "master")
+		return api.ParseWithMetadata(manifestContent, []string{}, 0, targetBranch)
+	}
+
 	if handler = registry.Get(uri); handler == nil {
 		return nil, fmt.Errorf("no suitable reader found for %s. Is this path correct?", uri)
 	}
