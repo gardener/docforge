90a50ba21da391f3c7c48d55390662fdd3e166a8
Viktor <mail.victor.kostov@gmail.com>
2024-03-13 17:25:35 +0200

    Better parameter name for linkresolver.ResolveLink

--- pkg/workers/linkresolver/link_resolving.go
+++ pkg/workers/linkresolver/link_resolving.go
@@ -39,22 +39,22 @@ type LinkResolver struct {
 }
 
 // ResolveLink resolves link
-func (l *LinkResolver) ResolveLink(destination string, node *manifest.Node, source string) (string, bool, error) {
-	escapedEmoji := strings.ReplaceAll(destination, "/:v:/", "/%3Av%3A/")
-	if escapedEmoji != destination {
-		klog.Warningf("escaping : for /:v:/ in link %s for source %s ", destination, source)
-		destination = escapedEmoji
+func (l *LinkResolver) ResolveLink(link string, node *manifest.Node, source string) (string, bool, error) {
+	escapedEmoji := strings.ReplaceAll(link, "/:v:/", "/%3Av%3A/")
+	if escapedEmoji != link {
+		klog.Warningf("escaping : for /:v:/ in link %s for source %s ", link, source)
+		link = escapedEmoji
 	}
-	destinationLink, err := url.Parse(destination)
+	linkURL, err := url.Parse(link)
 	if err != nil {
 		return "", false, fmt.Errorf("error when parsing link in %s : %w", source, err)
 	}
 	shouldValidate := true
 	// resolve outside links
-	if destinationLink.IsAbs() {
-		if _, err := l.Repositoryhosts.Get(destination); err != nil {
+	if linkURL.IsAbs() {
+		if _, err := l.Repositoryhosts.Get(link); err != nil {
 			// we don't have a handler for it. Leave it be.
-			return destination, true, nil
+			return link, true, nil
 		}
 	} else {
 		// convert destination to absolute link
@@ -62,33 +62,33 @@ func (l *LinkResolver) ResolveLink(destination string, node *manifest.Node, sour
 		if err != nil {
 			return "", false, fmt.Errorf("unexpected error - can't get a handler for already read content: %w", err)
 		}
-		if destination, err = docHandler.ToAbsLink(source, destination); err != nil {
+		if link, err = docHandler.ToAbsLink(source, link); err != nil {
 			if _, ok := err.(repositoryhosts.ErrResourceNotFound); !ok {
 				return "", false, err
 			}
-			klog.Warningf("failed to validate absolute link for %s from source %s: %v\n", destination, source, err)
+			klog.Warningf("failed to validate absolute link for %s from source %s: %v\n", link, source, err)
 			shouldValidate = false
 		}
 	}
 	// destination is absolute URL from a repository host
-	if !resource.IsResourceURL(destination) {
+	if !resource.IsResourceURL(link) {
 		// link is from repository host but not a resource
-		return destination, shouldValidate, nil
+		return link, shouldValidate, nil
 	}
 	// destination is resource URL
-	destinationLink, err = url.Parse(destination)
+	linkURL, err = url.Parse(link)
 	if err != nil {
-		return "", false, fmt.Errorf("unexpected error when parsing link %s in %s : %w", destination, source, err)
+		return "", false, fmt.Errorf("unexpected error when parsing link %s in %s : %w", link, source, err)
 	}
-	destinationResource, err := resource.FromURL(destinationLink)
+	destinationResource, err := resource.FromURL(linkURL)
 	if err != nil {
-		return "", false, fmt.Errorf("unexpected error when parsing link %s in %s : %w", destination, source, err)
+		return "", false, fmt.Errorf("unexpected error when parsing link %s in %s : %w", link, source, err)
 	}
 	destinationResourceURL := destinationResource.String()
 	// check if link refers to a node
 	nl, ok := l.SourceToNode[destinationResourceURL]
 	if !ok {
-		return destination, shouldValidate, nil
+		return link, shouldValidate, nil
 	}
 	// found nodes with this source -> find the shortest path from l.node to one of nodes
 	destinationNode := slices.MinFunc(nl, func(a, b *manifest.Node) int {
@@ -97,16 +97,16 @@ func (l *LinkResolver) ResolveLink(destination string, node *manifest.Node, sour
 		return cmp.Compare(strings.Count(relPathBetweenNodeAndA, "/"), strings.Count(relPathBetweenNodeAndB, "/"))
 	})
 	// construct destination from node path
-	destination = strings.ToLower(destinationNode.NodePath())
+	link = strings.ToLower(destinationNode.NodePath())
 	if l.Hugo.Enabled {
-		destination = strings.ToLower(destinationNode.HugoPrettyPath())
+		link = strings.ToLower(destinationNode.HugoPrettyPath())
 	}
-	destination = fmt.Sprintf("/%s/", path.Join(l.Hugo.BaseURL, destination))
-	if destinationLink.ForceQuery || destinationLink.RawQuery != "" {
-		destination = fmt.Sprintf("%s?%s", destination, destinationLink.RawQuery)
+	link = fmt.Sprintf("/%s/", path.Join(l.Hugo.BaseURL, link))
+	if linkURL.ForceQuery || linkURL.RawQuery != "" {
+		link = fmt.Sprintf("%s?%s", link, linkURL.RawQuery)
 	}
-	if destinationLink.Fragment != "" {
-		destination = fmt.Sprintf("%s#%s", destination, destinationLink.Fragment)
+	if linkURL.Fragment != "" {
+		link = fmt.Sprintf("%s#%s", link, linkURL.Fragment)
 	}
-	return destination, true, nil
+	return link, true, nil
 }
