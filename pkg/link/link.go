ef4f6b50d81ff3db9123ab7392f400a9e65f98ae
Viktor <v.kostov@sap.com>
2025-02-06 11:21:12 +0200

    Add comment to functions

--- pkg/link/link.go
+++ pkg/link/link.go
@@ -2,6 +2,7 @@ package link
 
 import "net/url"
 
+// Build builds a link given its elements
 func Build(elem ...string) (string, error) {
 	if len(elem) == 0 {
 		return "", nil
@@ -9,6 +10,7 @@ func Build(elem ...string) (string, error) {
 	return url.JoinPath(elem[0], elem[1:]...)
 }
 
+// MustBuild builds a link given its elements and panics if it fails
 func MustBuild(elem ...string) string {
 	res, err := Build(elem...)
 	if err != nil {
