3ad25592e04a23cbba72983ce412c02178394669
Viktor <v.kostov@sap.com>
2025-03-11 15:23:28 +0200

    pkg/manifestplugins/interface.go: Describe the value of a no-op NodeTransformation

--- pkg/manifestplugins/interface.go
+++ pkg/manifestplugins/interface.go
@@ -5,6 +5,9 @@ import "github.com/gardener/docforge/pkg/manifest"
 // Interface should be implemented by plugins
 type Interface interface {
 	// PluginNodeTransformations is the list of node transformations
-	// the plugin would like to apply when constructing the manifest
+	// the plugin would like to apply when constructing the manifest.
+	// Also plugins can provide a no-op transformation to get part of
+	// the node tree that will be needed when processing a node on a
+	// later stage
 	PluginNodeTransformations() []manifest.NodeTransformation
 }
