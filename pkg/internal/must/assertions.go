54191ff7db3ff46f9d705b0178e5a77c20ffbbd0
Viktor <v.kostov@sap.com>
2025-04-30 10:56:28 +0300

    pkg/internal/must/assertions.go: Improve assertion panic message

--- pkg/internal/must/assertions.go
+++ pkg/internal/must/assertions.go
@@ -1,6 +1,9 @@
 package must
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Package is inspired by https://github.com/tigerbeetle/tigerbeetle/blob/main/docs/TIGER_STYLE.md
 // "Assertions detect programmer errors. Unlike operating errors, which are expected and which must be handled, assertion failures are unexpected.
@@ -12,7 +15,7 @@ import "fmt"
 // Succeed panics on error.
 func Succeed[T any](obj T, err error) T {
 	if err != nil {
-		panic(fmt.Errorf("assertion failed: %w", err))
+		panic(fmt.Errorf("bug found through a failed assertion. Please create an issue containing the stack trace or open a PR fixing it: %w", err))
 	}
 	return obj
 }
@@ -20,13 +23,13 @@ func Succeed[T any](obj T, err error) T {
 // BeTrue panics if value is false
 func BeTrue(value bool) {
 	if !value {
-		panic(fmt.Sprintf("assertion failed: expected %t to be true", value))
+		panic(errors.New("bug found through a failed assertion. Please create an issue containing the stack trace or open a PR fixing it"))
 	}
 }
 
 // BeFalse panics if value is true
 func BeFalse(value bool) {
 	if value {
-		panic(fmt.Sprintf("assertion failed: expected %t to be false", value))
+		panic(errors.New("bug found through a failed assertion. Please create an issue containing the stack trace or open a PR fixing it"))
 	}
 }
